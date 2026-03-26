1d43d6d4d8ddeae4758df726cd3c279cf8fa0a46
Plumi9 <PhilipHoang02@gmail.com>
2026-03-26 13:40:03 +0100

    last commit, dead project

---
+++ stuff_desktop/TopDownHouse_SmallItems.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TopDownHouse_SmallItems" tilewidth="16" tileheight="16" tilecount="112" columns="14">
+ <image source="Assets/Top-Down_Retro_Interior/TopDownHouse_SmallItems.png" width="224" height="128"/>
+</tileset>
